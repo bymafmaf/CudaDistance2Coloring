51d3eda20d218cdfe901784b26062e4509d32783
Oran Can Oren <orancanoren@gmail.com>
2017-12-24 03:19:54 +0300

    bizim algoritma daha iyi abi

--- bfs.cu
+++ bfs.cu
@@ -11,19 +11,28 @@ const int N = 1024;
 
 char gfile[2048];
 
-using namespace::std;
+using namespace std;
+typedef unsigned int uint;
 
 void usage(){
-  printf("./bfs <filename> <sourceIndex>\n");
-  exit(0);
+	printf("./bfs <filename> <sourceIndex>\n");
+	exit(0);
 }
 // CUDA STARTS
 
+void checkCudaError(cudaError_t cudaError, const char * msg = "") {
+	if (cudaError != cudaSuccess) {
+		cerr << "CUDA error occured [" << msg << "]\nDescription: " << cudaGetErrorString(cudaError) << endl;
+		exit(1);
+	}
+}
 
 
 __global__
-void run(unsigned int *row_ptr, unsigned int *col_ind, unsigned int *results, int nov, unsigned int *errorCode);
+void assignColors(uint *row_ptr, int *col_ind, uint *results, int nov);
 
+__global__
+void detectConflicts(uint *row_ptr, int *col_ind, uint *results, int nov, bool *errorCode);
 
 /*
 You can ignore the ewgths and vwghts. They are there as the read function expects those values
@@ -31,84 +40,96 @@ row_ptr and col_ind are the CRS entities. nov is the Number of Vertices
 */
 
 int main(int argc, char *argv[]) {
-  unsigned int *row_ptr;
-  int *col_ind;
-  ewtype *ewghts;
-  vwtype *vwghts;
-  int nov;
+	cudaError_t cudaError;
+	// ==== HOST MEMORY ====
+	uint *row_ptr;
+	int *col_ind;
+	ewtype *ewghts;
+	vwtype *vwghts;
+	int nov;
+	uint * results;
+	bool * errorCode = new bool;
+	*errorCode = true;
+	
+	if(argc != 2)
+	usage();
+	
+	const char* fname = argv[1];
+	strcpy(gfile, fname);
+	
+	if(read_graph(gfile, &row_ptr, &col_ind, &ewghts, &vwghts, &nov, 0) == -1) {
+		printf("error in graph read\n");
+		exit(1);
+	}
 
-  if(argc != 2)
-  usage();
+	results = (uint *) malloc(nov * sizeof(uint)); // will store color number for each
+	for (int i = 0; i < nov; i++) {
+		results[i] = 1;
+	}
+	// ===== DEVICE MEMORY =====
+	uint *d_row_ptr;
+	int *d_col_ind;
+	uint *d_results;
+	const size_t row_size = (nov + 1) * sizeof(uint);
+	const size_t col_size = (row_ptr[nov]) * sizeof(int);
+	bool * d_errorCode; // true, if conflict detected; false otherwise
+	
+	cudaError = cudaMalloc((void **)&d_errorCode, sizeof(bool));
+	checkCudaError(cudaError, "malloc errorCode");
+	cudaError = cudaMalloc((void **)&d_row_ptr, row_size);
+	checkCudaError(cudaError, "malloc d_row_ptr");
+	cudaError = cudaMalloc((void **)&d_col_ind, col_size);
+	checkCudaError(cudaError, "malloc d_col_ind");
+	cudaError = cudaMalloc((void **)&d_results, nov * sizeof(uint));
+	checkCudaError(cudaError, "malloc d_results");
+	
+	cudaError = cudaMemcpy(d_results, results, nov * sizeof(uint), cudaMemcpyHostToDevice);
+	checkCudaError(cudaError, "HtoD memcpy results");
+	cudaError = cudaMemcpy(d_row_ptr, row_ptr, row_size, cudaMemcpyHostToDevice);
+	checkCudaError(cudaError, "HtoD memcpy row_ptr");
+	cudaError = cudaMemcpy(d_col_ind, col_ind, col_size, cudaMemcpyHostToDevice);
+	checkCudaError(cudaError, "HtoD memcpy col_ind");
 
-  const char* fname = argv[1];
-  strcpy(gfile, fname);
+	// ==== KERNEL LAUNCH =====
+	const uint numBlocks = (nov + N - 1) / N;
+	const uint numThreadsPerBlock = N;
 
-  if(read_graph(gfile, &row_ptr, &col_ind, &ewghts, &vwghts, &nov, 0) == -1) {
-    printf("error in graph read\n");
-    exit(1);
-  }
-  /****** YOUR CODE GOES HERE *******/
-  std::cout << "34 " << row_ptr[34] << '\n';
-
-  unsigned int *d_row_ptr;
-  unsigned int *d_col_ind;
-  unsigned int *d_results;
-  int row_size = nov * sizeof(unsigned int);
-  int col_size = (row_ptr[nov] + 1) * sizeof(int);
-
-  unsigned int *results = (unsigned int *) malloc(row_size);
-  cudaMalloc((void **)&d_row_ptr, row_size);
-  cudaMalloc((void **)&d_col_ind, col_size);
-  cudaMalloc((void **)&d_results, row_size);
-
-
-  for (size_t i = 0; i < nov; i++) {
-    for (size_t j = row_ptr[i]; j < row_ptr[i+1]; j++) {
-      unsigned int d1index = col_ind[j];
-      results[d1index] = row_ptr[i+1] - j + 1;
-    }
-  }
-
-  for (size_t i = 0; i < nov; i++) {
-    printf("%d %d\n", i, results[i]);
-  }
-
-  cudaMemcpy(d_results, results, row_size, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_row_ptr, row_ptr, row_size, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_col_ind, col_ind, col_size, cudaMemcpyHostToDevice);
-
-  unsigned int *d_errorCode;
-  cudaMalloc((void **)&d_errorCode, sizeof(unsigned int));
-  unsigned int *errorCode = (unsigned int*)malloc(sizeof(unsigned int));
-  unsigned int *zero = (unsigned int*)malloc(sizeof(unsigned int));
-  zero[0] = 0;
-  errorCode[0] = 1;
-  int k = 0;
-  while (errorCode[0] != 0) {
-    cudaMemcpy(d_errorCode, zero, sizeof(unsigned int), cudaMemcpyHostToDevice);
-    run<<<(nov + N -1) / N, N>>>(d_row_ptr, d_col_ind, d_results, nov, d_errorCode);
-    k++;
-    cudaMemcpy(errorCode, d_errorCode, sizeof(unsigned int), cudaMemcpyDeviceToHost);
-  }
-
-
-  cudaMemcpy(results, d_results, row_size, cudaMemcpyDeviceToHost);
-
-  // TODO use results
-  unsigned int max = 0;
-  for (size_t i = 0; i < nov; i++) {
-    if (results[i] > max) {
-      max = results[i];
-    }
-  }
-  std::cout << "max is " << max << '\n';
-
-  cudaFree(d_row_ptr);
-  cudaFree(d_col_ind);
-  cudaFree(d_results);
-
-  free(row_ptr);
-  free(col_ind);
-
-  return 1;
+	int iterationCounter = 0; // for the following loop
+	printf("running kernel with %d blocks with %d threads each\n", numBlocks, numThreadsPerBlock);
+	while (*errorCode) { // run kernel until no conflict occurs
+		*errorCode = false;
+		cudaError = cudaMemcpy(d_errorCode, errorCode, sizeof(bool), cudaMemcpyHostToDevice);
+		checkCudaError(cudaError, "memcpy errorCode");
+		assignColors<<< numBlocks, numThreadsPerBlock >>>(d_row_ptr, d_col_ind, d_results, nov);
+		cudaDeviceSynchronize();
+		checkCudaError(cudaGetLastError(), "assignColors() error");
+		detectConflicts<<< numBlocks, numThreadsPerBlock >>>(d_row_ptr, d_col_ind, d_results, nov, d_errorCode);
+		cudaDeviceSynchronize();
+		checkCudaError(cudaGetLastError(), "detectConflicts() error");
+		cudaError = cudaMemcpy(errorCode, d_errorCode, sizeof(bool), cudaMemcpyDeviceToHost);
+		checkCudaError(cudaError);
+		iterationCounter++;
+		cout << "iteration " << iterationCounter << endl;
+	}
+	
+	cudaError = cudaMemcpy(results, d_results, nov*sizeof(uint), cudaMemcpyDeviceToHost);
+	checkCudaError(cudaError, "DtoH memcpy results");
+	
+	// TODO use results
+	uint max = 0;
+	for (size_t i = 0; i < nov; i++) {
+		if (results[i] > max) {
+			max = results[i];
+		}
+	}
+	std::cout << "max is " << max << '\n';
+	
+	cudaFree(d_row_ptr);
+	cudaFree(d_col_ind);
+	cudaFree(d_results);
+	
+	free(row_ptr);
+	free(col_ind);
+	
+	return 1;
 }
--- cudabfs.cu
+++ cudabfs.cu
@@ -1,67 +1,63 @@
 #include <stdio.h>
 #include "graph.h"
 
+typedef unsigned int uint;
+
 __global__
-void run(unsigned int *row_ptr, unsigned int *col_ind, unsigned int *results, int nov, unsigned int *errorCode) {
-  const unsigned int vIndex = blockIdx.x * blockDim.x + threadIdx.x;
-  if (vIndex == 0) {
-    printf("%s\n", "basla");
-    for (size_t i = 0; i < nov; i++) {
-      printf("%d %d\n", i, results[i]);
-    }
-  }
-  // if (vIndex < nov) {
-  //   unsigned int count = 1;
-  //   for (size_t i = row_ptr[vIndex]; i < row_ptr[vIndex+1]; i++) {
-  //     unsigned int d1Index = col_ind[i];
-  //     if (row_ptr[d1Index+1] < row_ptr[d1Index]) {
-  //       printf("d1Index+1 val %d\n", row_ptr[d1Index+1]);
-  //       printf("d1Index val %d\n", row_ptr[d1Index]);
-  //       printf("d1Index+1 %d\n", d1Index+1);
-  //       printf("d1Index %d\n", d1Index);
-  //     }
-  //     count += row_ptr[d1Index+1] - row_ptr[d1Index] + 1;
-  //   }
-  //
-  //   unsigned int *forbiddenArray = new unsigned int[count];
-  //   printf("count %d\n", count);
-  //   unsigned int selfColor = results[vIndex];
-  //   unsigned int forbiddenIndex = 0;
-  //   unsigned int conflictExists = 0;
-  //   for (size_t i = row_ptr[vIndex]; i < row_ptr[vIndex+1]; i++) {
-  //     unsigned int d1Index = col_ind[i];
-  //     forbiddenArray[forbiddenIndex] = results[d1Index];
-  //     if (forbiddenArray[forbiddenIndex] == selfColor) {
-  //       conflictExists = 1;
-  //     }
-  //     forbiddenIndex++;
-  //     for (size_t j = row_ptr[d1Index]; j < row_ptr[d1Index+1]; j++) {
-  //       unsigned int d2Index = col_ind[j];
-  //       if (d2Index != vIndex) {
-  //         forbiddenArray[forbiddenIndex] = results[d2Index];
-  //         if (forbiddenArray[forbiddenIndex] == selfColor) {
-  //           conflictExists = 1;
-  //         }
-  //         forbiddenIndex++;
-  //       }
-  //     }
-  //   }
-  //
-  //   if (conflictExists == 1) {
-  //     for (size_t i = 1; i < nov; i++) {
-  //       unsigned int found = 0;
-  //       for (size_t j = 0; j < count; j++) {
-  //         if (forbiddenArray[j] == i) {
-  //           found = 1;
-  //           break;
-  //         }
-  //       }
-  //       if (found != 1) {
-  //         results[vIndex] = i;
-  //         break;
-  //       }
-  //     }
-  //     atomicMax(errorCode, 1);
-  //   }
-  // }
+void assignColors(uint *row_ptr, int *col_ind, uint *results, int nov) {
+	const uint vIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	if (vIndex < nov && results[vIndex] == 0) {
+		uint selectedColor = 1;
+		for (size_t i = row_ptr[vIndex]; i < row_ptr[vIndex+1]; i++) { // distance-1 neighbor loop
+			uint d1Index = col_ind[i];
+			if (selectedColor == results[d1Index]) {
+				selectedColor++;
+				i = row_ptr[vIndex] - 1; // reset the loop
+				continue;
+			}
+			for (size_t j = row_ptr[d1Index]; j < row_ptr[d1Index+1]; j++) {
+				uint d2Index = col_ind[j];
+				if (selectedColor == results[d2Index]) {
+					i = row_ptr[vIndex] - 1; // reset the loop
+					break;
+				}
+			}
+		}
+
+	}
+}
+// && results[vIndex] != 0
+__global__
+void detectConflicts(uint *row_ptr, int *col_ind, uint *results, int nov, bool * errorCode) {
+	const uint vIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	if (vIndex < nov) {
+		bool secondDistanceConflictFound = false;
+		for (size_t i = row_ptr[vIndex]; i < row_ptr[vIndex+1] && !secondDistanceConflictFound; i++) { // distance-1 neighbor loop
+			uint d1Index = col_ind[i];
+			if (results[vIndex] == results[d1Index]) {
+				if (vIndex < d1Index) {
+					results[vIndex] = 0;
+				}
+				else {
+					results[d1Index] = 0;
+				}
+				break;
+			}
+			for (size_t j = row_ptr[d1Index]; j < row_ptr[d1Index+1] && !secondDistanceConflictFound; j++) {
+				uint d2Index = col_ind[j];
+				if (results[vIndex] == results[d2Index]) {
+					if (vIndex < d2Index) {
+						results[vIndex] = 0;
+					}
+					else {
+						results[d2Index] = 0;
+					}
+					secondDistanceConflictFound = true;
+				}
+			}
+		}
+		if (results[vIndex] == 0) {
+			*errorCode = true;
+		}
+	}
 }
